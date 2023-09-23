075e20829f286fc152bd4a282ece3badafb83fef
BernardEpp <bernard.epp@acommo.com>
2023-09-23 23:34:29 +0200

    implemented expiration completed listener

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,36 @@
+import { Listener, ExpirationCompleteEvent, Subjects } from "@bernard-tickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order, OrderStatus } from "../../models/order";
+import { OrderCancelledEvent } from "@bernard-tickets/common";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { isMissingDeclaration } from "typescript";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      // NOTE: we don't need to reset the ticket here as this is handled in the ticket model in the isReserved method
+    });
+    await order.save();
+
+    new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      }
+    });
+
+    msg.ack();
+  }
+}
